57ffe9425da270369c4d0b1e08da819130d3d692
bekjan <bekambkg@gmail.com>
2020-04-12 18:29:54 +0600

    added errors handling 404 and 500

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
+import logging
+from logging.handlers import SMTPHandler
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'mysql://admin:admin@localhost/microblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['bekambkg@gmail.com']
